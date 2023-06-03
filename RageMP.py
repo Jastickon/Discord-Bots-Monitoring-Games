853fb2845e39e6c087656c66db5a382c5002dae0
Igor20264 <76581675+Igor20264@users.noreply.github.com>
2023-06-03 23:15:07 +0300

    Update RageMP.py

--- RageMP.py
+++ RageMP.py
@@ -1,13 +1,17 @@
 import requests
-
-#[1.1]","gamemode":"roleplay","url":"https://blrevived.de","lang":"ge","players":1,"peak":1,"maxplayers":300},"es.gta5grand.com:22005":{"name":"рџ›‘[ES][voice] [roleplay] Grand RolePlay | discord.gg/gta5grandcom
-
 ip = "es.gta5grand.com:22005"
 
-def getPlayers(ip):
+def getPlayers(ip:str) -> tuple:
+    """
+
+    :param ip: "es.gta5grand.com:22005" or "8.8.8.8:8080"
+    :return: (cur_player | max | peak)
+    """
     s = requests.get("https://cdn.rage.mp/master")
-    data = master.loads(s.content)
-    for i in data:
-        if i["ip"] == ip:
-            print(i)
-            return i["players"],i["maxplayers"]
+    data = s.content
+    conData = eval(data)
+    for i in conData:
+        if i == ip:
+            data =conData[i]
+            return data['players'],data['maxplayers'],data['peak']
+getPlayers(ip)
